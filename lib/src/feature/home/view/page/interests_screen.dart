389f27b6e588978edc0baa9e13421bed40e4d282
Eduardo Alvim <dudualvim20@gmail.com>
2023-05-01 13:45:40 -0300

    adicionando a tela d interesses

--- lib/src/feature/home/view/page/interests_screen.dart
+++ lib/src/feature/home/view/page/interests_screen.dart
@@ -9,57 +9,133 @@ class InterestsScreen extends StatefulWidget {
 
 class InterestsScreenState extends State<InterestsScreen> {
   List<String> interests = [
-    'Arquitetura',
-    'Artesanato',
-    'Aventura',
-    'Culinária',
-    'História',
-    'Música',
-    'Natureza',
-    'Paisagem',
-    'Passeios de barco',
-    'Praia',
-    'Relaxamento',
+    'Adventure',
+    'Amusement parks',
+    'Archaeological sites',
+    'Architecture',
+    'Art',
+    'Beaches',
+    'Bike tours',
+    'Botanical gardens',
+    'Brewery',
+    'Churches',
+    'Climbing',
+    'Coral reefs',
+    'Culture',
+    'Dance',
+    'Ecotourism',
+    'Events',
+    'Family vacations',
+    'Festivals',
+    'Forests',
+    'Gastronomy',
+    'Golf',
+    'Helicopter tours',
+    'Historical sites',
+    'History',
+    'Hiking',
+    'Islands',
+    'Kitesurfing',
+    'Local experiences',
+    'Luxury',
+    'Markets',
+    'Mountain',
+    'Museums',
+    'Natural wonders',
+    'Nature',
+    'Nightlife',
+    'National parks',
+    'Ocean',
+    'Photography',
+    'Relaxation',
+    'Religious tourism',
+    'Resorts',
+    'River',
     'Romance',
-    'Surfe',
-    'Vida noturna',
-    'Vida selvagem',
-    'Vinícolas',
+    'Rural tourism',
+    'Ruins',
+    'Safaris',
+    'Sanctuaries',
+    'Shopping',
+    'Sightseeing tours',
+    'Skiing',
+    'Snowboarding',
+    'Spirituality',
+    'Spa',
+    'Surfing',
+    'Theater',
+    'Train tours',
+    'Villages',
+    'Volcanoes',
+    'Water parks',
+    'Wildlife',
+    'Wine tasting',
   ];
   List<String> selectedInterests = [];
+
+  bool get isButtonEnabled => selectedInterests.length >= 3;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          color: Color(0xFF0096C7),
+          color: Colors.white,
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Wrap(
-            spacing: 8.0,
-            runSpacing: 8.0,
-            children: List<Widget>.generate(
-              interests.length,
-              (int index) {
-                final interest = interests[index];
-                return ChoiceChip(
-                  label: Text(interest),
-                  selected: selectedInterests.contains(interest),
-                  onSelected: (bool selected) {
-                    setState(() {
-                      if (selected) {
-                        selectedInterests.add(interest);
-                      } else {
-                        selectedInterests.remove(interest);
-                      }
-                    });
-                  },
-                );
-              },
-            ).toList(),
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Change your interests',
+                style: TextStyle(fontSize: 24, color: Color(0xFF0096C7)),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      spacing: 8.0,
+                      runSpacing: 8.0,
+                      children: List<Widget>.generate(
+                        interests.length,
+                        (int index) {
+                          final interest = interests[index];
+                          return ChoiceChip(
+                            selected: selectedInterests.contains(interest),
+                            label: Text(
+                              interest,
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: selectedInterests.contains(interest)
+                                    ? Colors.white
+                                    : Colors.black,
+                              ),
+                            ),
+                            selectedColor: Colors.orange.shade700,
+                            onSelected: (bool selected) {
+                              setState(() {
+                                if (selected) {
+                                  selectedInterests.add(interest);
+                                } else {
+                                  selectedInterests.remove(interest);
+                                }
+                              });
+                            },
+                          );
+                        },
+                      ).toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
